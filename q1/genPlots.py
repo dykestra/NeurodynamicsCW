7df49891db2f61bbb5971e99f937e9e3569b1e7e
Alan <alan.vey@gmail.com>
2015-11-17 18:12:08 +0000

    Style improvement.

--- q1/genPlots.py
+++ q1/genPlots.py
@@ -1,6 +1,6 @@
-from PlotConnectivity import Scatter as Lattice
+from PlotConnectivity import Scatter
 from Connectivity import Connectivity as Conn
 from numpy import arange
 
 for i in arange(0, 0.6, 0.1):
-  Lattice(Conn(i), i)
+  Scatter(Conn(i), i)
